2ba41b1af948583095735282831032d625f92a56
cech92 <cech92@live.it>
2021-10-20 00:48:47 +0200

    fix settings

--- backend/planetly_test/settings.py
+++ backend/planetly_test/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=300),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
